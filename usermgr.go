d61b1d01e621d2b74a92890b31d583d5c20d46c0
LeeTrent <Lee.Trent.1@gmail.com>
2017-04-01 15:12:47 -0500

    Encapsulates user map inside a struct

--- usermgr.go
+++ usermgr.go
@@ -6,31 +6,46 @@ import (
 )
 
 type User struct {
-	UserName string
-	Password []byte
-	First    string
-	Last     string
+	UserName  string
+	Password  []byte
+	FirstName string
+	LastName  string
 }
-type UserMgr map[string]User
 
-func (um UserMgr) GetUser(un string) User {
-	return um[un]
+type UserMgr struct {
+	userMap map[string]User
+}
+
+func NewUserMgr() UserMgr {
+	um := UserMgr{
+		userMap: make(map[string]User),
+	}
+	return um
+}
+
+func (um UserMgr) GetUser(un string) (User, bool) {
+	user, found := um.userMap[un]
+	if found {
+		return user, found
+	} else {
+		return User{}, found
+	}
 }
 
 func (um UserMgr) UserNameIsTaken(un string) bool {
-	_, ok := um[un]
+	_, ok := um.userMap[un]
 	return ok
 }
 
-func (um UserMgr) RemoveUser(un string) {
-	delete(um, un)
+func (um UserMgr) RemoveUser(userName string) {
+	delete(um.userMap, userName)
 }
 
 func (um UserMgr) CreateUser(pw []byte, un, fn, ln string) (User, error) {
-	if _, ok := um[un]; ok {
+	if _, ok := um.userMap[un]; ok {
 		errMsg := fmt.Sprintf("Username '%s' already taken", un)
 		return User{}, errors.New(errMsg)
 	}
-	um[un] = User{un, pw, fn, ln}
-	return um[un], nil
+	um.userMap[un] = User{un, pw, fn, ln}
+	return um.userMap[un], nil
 }
